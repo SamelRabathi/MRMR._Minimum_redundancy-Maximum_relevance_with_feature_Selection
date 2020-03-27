56862eeee6024b5326739c40780eed246856a135
Samel Rabathi <rabathi.samel@gmail.com>
2020-03-27 10:26:25 +0100

    Waren entweder zur vorzeitigen Strukturierung, die durch Trennung der jeweiligen Teilaufgaben vollzogen wurde, oder haben bezüglich der Aufgaben keine Verwendung

--- Python Code/Maximum Relevance.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  9 17:40:29 2020
-
-@author: samelrabathi
-"""
-
--- Python Code/Minumum Redundancy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  9 17:41:29 2020
-
-@author: samelrabathi
-"""
-
